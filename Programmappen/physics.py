a94dcc81b2b02b6eaf38681b9513c16480f65667
Johan Skak <qrd245@ku.dk>
2025-10-14 10:51:09 +0200

    New test

--- Programmappen/physics.py
+++ Programmappen/physics.py
@@ -1,5 +1,6 @@
 import numpy as np
 from scipy.interpolate import CubicSpline
+import os
 
 
 # ---------------- Physical / model constants ----------------
@@ -183,7 +184,7 @@ def heat_capacity_profile(x, T, k1):
     #                         0.4,  # 50N -> Eurasia + N. America dominate, but N. Atlantic/Pacific present
     #                         0.65,  # 70N -> Arctic Ocean exists, though partly enclosed
     #                         1.0])  # Central Arctic Ocean basin
-    data = np.loadtxt("ocean_fraction_by_latitude_5deg.csv", delimiter=",", skiprows=1)
+    data = np.loadtxt(os.path.join(os.path.dirname(__file__), 'ocean_fraction_by_latitude_5deg.csv'), delimiter=",", skiprows=1)
     lat_knots_f = data[:,0]
     f_ocean_knots = data[:,1]
     
